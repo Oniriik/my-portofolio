c50e142d99908189cb63c59b4129a459825d7096
unknown <timot5924@gmail.com>
2022-05-16 03:17:29 +0200

    added laboiteamessage

--- src/components/DisplayProjects.vue
+++ src/components/DisplayProjects.vue
@@ -5,7 +5,7 @@
       img="https://i.ibb.co/mHVRypS/spotify.png"
       stack="Vuejs v3, Vuex"
       desc="Spotify API based app that allows users to get their top tracks in a picture form to share on socials."
-      href="https://spotify-dump.xyz/"
+      href="https://github.com/Oniriik/spotify-dump"
       accentColor="#2FBF57"
       secondColor="#BFADD5"
     />
@@ -21,18 +21,27 @@
     <ElementProject
       title="Allo'Cinoche"
       img="https://i.ibb.co/znQrRwH/allocinoche.png"
-      stack="ExpressJs, Ejs, Strapi"
+      stack="ExpressJs, Ejs, Strapi API"
       desc="Website that reference movies/series. Users can add items via + button. It use movie-database-alternative to add new content."
-      href="https://github.com/Oniriik/U-Bordeaux-Verif"
+      href="https://github.com/Oniriik/allocinoche"
       accentColor="#8E3248"
-      secondColor="#7979cc"
+      secondColor="#F5C056"
+    />
+    <ElementProject
+      title="La boite à message"
+      img="https://i.ibb.co/tCXMwj4/letter.jpg"
+      stack="Python, Selenium, Tweepy, Strapi API"
+      desc="Send message anonymously on Instagram and twitter"
+      href="https://github.com/Oniriik/laboiteamessage"
+      accentColor="#DA427D"
+      secondColor="#8ad890"
     />
     <ElementProject
       title="My portofolio"
       img="https://i.ibb.co/Vj2khvH/folio.png"
       stack="Vuejs v3"
       desc="My personnal portofolio made from scratch."
-      href="#"
+      href="https://github.com/Oniriik/my-portofolio"
       accentColor="#79ccb2"
       secondColor="#F7A59D"
     />
